5fae0d70e17d80863930ceb7a023dc7c28ba4370
ColdxP <laikimbler@gmail.com>
2017-04-21 20:34:35 -0300

    Revert "Revert "Cold_Rakan""
    This reverts commit b833a1b8a3ca57d49e38186a4e6c513823546db5.

---
+++ Cold_Rakan/MenuAdmin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace Cold_Rakan
+{
+    internal class MenuAdmin
+    {
+        public static Menu ColdRakan;
+        public static Menu DrawMenu;
+        public static Menu ComboMenu;
+        public static Menu HarassMenu;
+
+        public static void CreateMenu()
+        {
+            ColdRakan = MainMenu.AddMenu("Cold_Rakan", "Cold_Rakan");
+
+            ComboMenu = ColdRakan.AddSubMenu("Combo");
+            ComboMenu.Add("Q", new CheckBox("Use Q", true));
+            ComboMenu.Add("W", new CheckBox("Use W", true));
+            ComboMenu.Add("R", new CheckBox("Use R", false));
+            ComboMenu.Add("Ignite", new CheckBox("Use Ignite", true));
+
+            HarassMenu = ColdRakan.AddSubMenu("Harass");
+            HarassMenu.Add("Q", new CheckBox("Use Q", true));
+
+            DrawMenu = ColdRakan.AddSubMenu("Draw");
+            DrawMenu.Add("Q", new CheckBox("Draw Q", true));
+            DrawMenu.Add("W", new CheckBox("Draw W", true));
+            DrawMenu.Add("E", new CheckBox("Draw E", true));
+            DrawMenu.Add("R", new CheckBox("Draw R", true));
+        }
+
+    }
+}
---
+++ Cold_Rakan/Spells.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Spells;
+
+namespace Cold_Rakan
+{
+    internal class Spells
+    {
+        public static Spell.Skillshot Q;
+        public static Spell.Skillshot W;
+        public static Spell.Targeted E;
+        public static Spell.Active R;
+        public static Spell.Targeted Ignite;
+
+        static Spells()
+        {
+            Q = new Spell.Skillshot(SpellSlot.Q, 750, SkillShotType.Linear, 0, 150, 75, DamageType.Magical);
+            Q.AllowedCollisionCount = 0;
+            W = new Spell.Skillshot(SpellSlot.W, 600, SkillShotType.Circular, 0, int.MaxValue, 250, DamageType.Magical);
+            W.AllowedCollisionCount = int.MaxValue;
+            E = new Spell.Targeted(SpellSlot.W, 550);
+            R = new Spell.Active(SpellSlot.R, 200);
+            Ignite = new Spell.Targeted(SummonerSpells.Ignite.Slot, 600);
+        }
+
+    }
+}
